2d6f9deab478241fcc8eef825680dbca3ef544d0
Jeremy Porter <Jeremy Porter>
2016-12-01 11:59:49 -0700

    made some changes for viewing

--- js/home/home.js
+++ js/home/home.js
@@ -1,5 +1,27 @@
 angular.module('app')
-.controller('homeCtrl', function($scope){
+.controller('homeCtrl', ['$location', '$anchorScroll',
+ function($scope, $location, $anchorScroll){
     $scope.test = "TESTING!!!";
 
-});
+$scope.JumpToTop = function () {
+    console.log("jump to top");
+    $location.hash('menu');
+    $anchorScroll();
+}
+
+$scope.JumpToSkills = function () {
+    $location.hash('slide1');
+    $anchorScroll();
+}
+
+$scope.JumpToBio = function () {
+    $location.hash('slide2');
+    $anchorScroll();
+}
+
+$scope.JumpToContact = function () {
+    $location.hash('slide3');
+    $anchorScroll();
+}
+
+}]);
